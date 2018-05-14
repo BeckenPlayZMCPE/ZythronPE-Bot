1dae8c674d7985d306393554b6983fc3ae041021
BeckenPlayZMCPE <beckencraft123@gmail.com>
2018-05-14 19:35:32 +0500

    Update implade.js

--- implade.js
+++ implade.js
@@ -3,32 +3,32 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('for final exam on school!', {type: 'WATCHING'});
+    client.user.setActivity('play.zythronpe.ml:19132', {type: 'PLAYING'});
 });
 
 client.on('message', async msg => {
     if(msg.author.bot) return;
     
     if (msg.content === '$help') {
-    	msg.channel.send('**__Commands List__** ```[ $server | $info | $ghostview | $help | $github ]``````[ $version | $dcinvite ]```');
+    	msg.channel.send('**__Commands List__** ```[ $ip | $info | $help ]``````[ $version | $dcinvite ]```');
     } else
-        if (msg.content === 'Zade') {
-    	msg.channel.send('You called him. Please tell, what do you want to talk about?');
+        if (msg.content === 'Becken') {
+    	msg.channel.send('You called the owner. Looks like you want to talk about an important thing.');
     } else
         if (msg.content === '$ghostview') {
     	msg.channel.send('**Download the latest plugins builds on Poggit at:** __http://poggit.pmmp.io/ci/Zadezter/GhostView__``` Poggit ¡ | Github ! ```**Check all source code what is made by Zadezter on Github at:** __http://github.com/Zadezter/GhostView__');
     } else
-        if (msg.content === '$server') {
-    	msg.channel.send('**This is the ZythronPE server! Come and play if you have a time!** ```IP: play.zythronpe.ml | Port: 19132```');
+        if (msg.content === '$ip') {
+    	msg.channel.send('```IP: play.zythronpe.ml | Port: 19132```');
     } else
         if (msg.content === '$info') {
-    	msg.channel.send('**This bot was created on 6 May 2018 by using code with Discord.js!** [Owner: __Zadezter#0207__]');
+    	msg.channel.send('**This server was created by BeckenPlayZMCPE on 19th January 2018 and BeckenPlayZMCPE was helped by the two staff MrLukePotatoes and Zadezter in managing the server and it took about 3 months to release the server. This server is a multi-server and if you wonder what multiserver means then it means all in one like factions,minigames,roleplay,kitpvp everything in a single server. If you ever wanted to apply for staff here, go to bit.ly/ZPEApply. Also check out our twitter page @ZythronPE and make sure to follow. If you have any questions regarding the server< make sure to contact @BeckenPlayZMCPE and he will answer you as soon as he can.');
     } else
         if (msg.content === '$version') {
-        msg.channel.send('**This bot is running a language code** __discord.js__ **on version IB-1.3 with using a Heroku Server Hosting 24/7!**');
+        msg.channel.send('**This server is running on mcpe version v1.2.13/1.2.14');
     } else
         if (msg.content === '$dcinvite') {
-        msg.channel.send('**ImpladeDeveloped is a starter team for making a code of their bot and public/private plugins!** Tap this link and join: https://discord.gg/2pRGG52');
+        msg.channel.send('**If you ever wonder what is the invitation link to ZythronPE's discord server. Here it is:** Tap this link and join: https://discord.gg/5brHyJa');
     } else
         if (msg.content === '$github') {
         msg.channel.send('**This is our ImpladeDeveloped team on Github!** Tap this link: http://github.com/ImpladeDeveloped');
